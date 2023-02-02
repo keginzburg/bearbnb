5207f88c7316e9fed3639414fe2ec7abc445c847
Kyle Ginzburg <keginzburg@gmail.com>
2023-02-02 17:45:00 -0500

    Add backend and frontend casing config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Responses created on the backend need to be converted from snake_case to camelCase
+# Jbuilder configuration:
+Jbuilder.key_format camelize: :lower
+Jbuilder.deep_format_keys true
